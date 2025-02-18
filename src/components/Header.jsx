331096f773e17866d88d54a20380bb4a3f4d3957
Hector Acevedo <HEA092024@gmail.com>
2025-02-18 10:44:19 +0100

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 
 export default function Header ({data : {addTask}}) {
     const [newTask, setNewTask] = useState("")
+
+    function handleAddTask () { 
+
+    }
     return (
         <div>
             <h1>TODO List</h1>
-            <form action="">
+            <form onSubmit={handleAddTask}>
             <input type="text" />
-            <button>Submit task</button>
+            <button type='submit'>Submit task</button>
             </form>
         </div>
     )
