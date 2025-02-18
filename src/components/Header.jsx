977a19e21219496f16d5e81ac515ac76a812d6a6
Hector Acevedo <HEA092024@gmail.com>
2025-02-18 10:37:16 +0100

    added input + button inside form tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
-export default function Header () {
+export default function Header ({data : {addTask}}) {
     return (
         <div>
             <h1>TODO List</h1>
+            <form action="">
+            <input type="text" />
+            <button>Submit task</button>
+            </form>
         </div>
     )
 }
