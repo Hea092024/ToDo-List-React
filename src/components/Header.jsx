585f6008e94f185f60a9955aa51aaaacc62b7386
Hector Acevedo <HEA092024@gmail.com>
2025-02-19 10:32:29 +0100

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { v4 as uuid } from 'uuid';
 
-export default function Header ({data : {addTask}}) {
+export default function Header ({data : {addTask, setSortOption}}) {
     const [newTaskName, setNewTaskName] = useState("")
 
     function handleAddTask (e) { 
@@ -23,7 +23,7 @@ export default function Header ({data : {addTask}}) {
           <input type="text" onChange={(e) => setNewTaskName(e.target.value)} />
           <button type="submit">Submit task</button>
         </form>
-        <select name="" id="">
+        <select name="" id="" onChange={() => setSortOption(prev =>({...prev, sortBy: e.target.value}))}>
           <option value="newest">Newest to Oldest</option>
           <option value="oldest">Oldest to Newest</option>
           <option value="a-to-z">A to Z</option>
