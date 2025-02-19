3604cc852028044a25f56b5f8d94b22dcc2ced96
Hector Acevedo <HEA092024@gmail.com>
2025-02-19 09:46:39 +0100

    updated edit task

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 export default function TodoItem ({data: {task, editTask, deleteTask}}) {
 const [isReadOnly, setISReadOnly] = useState(true);
+const [updatedTaskName , setUpdatedTaskName] = useState(task.name);
     function changeCompleted () {
 
     }
 
     const formattedTime = task.timestamp.toLocaleString("en-GB");
     function handleEdit () {
-
+if(!isReadOnly) {
+  editTask(task.id, {...task, name: updatedTaskName});
+}
+setISReadOnly(prev => !prev)
     }
 
     return (
@@ -18,8 +22,8 @@ const [isReadOnly, setISReadOnly] = useState(true);
           onChange={changeCompleted}
         />
         <p>{formattedTime}</p>
-        <input type="text" value={task.name} readOnly={isReadOnly}/>
-<button onClick={handleEdit}>Edit</button>
+        <input type="text" value={updatedTaskName} readOnly={isReadOnly} onChange ={() => setUpdatedTaskName(e.target.value)}/>
+<button onClick={handleEdit}>{isReadOnly ? "Edit" : "Save"}</button>
 <button onClick={() => deleteTask(task.id)}>Delete</button>
       </li>
     );
